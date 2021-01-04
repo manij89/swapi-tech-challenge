d4abd9e0a0195c797d7bebdab96f27e987a78ebb
Mandy Nijssen <nijssenmandy89@gmail.com>
2021-01-04 11:30:48 +0000

    fix(index): change relative path

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const path = require('path');
-const publicPath = path.join(__dirname, 'client/build');
+
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(publicPath));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 const port = process.env.PORT || 4002;
