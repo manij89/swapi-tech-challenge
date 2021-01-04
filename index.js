3a6809d09f4bb4c11f50b99d5b04f0c8f345e4eb
Mandy Nijssen <nijssenmandy89@gmail.com>
2021-01-04 10:59:49 +0000

    build(index.js): add .env and changed relative path to build file

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const publicPath = path.join(__dirname, './client/build');
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(publicPath));
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-const port = 4002;
+const port = process.env.PORT || 4002;
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port} 🌈🌈🌈`); //eslint-disable-line no-console
 });
